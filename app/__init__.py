f46e55536ba99fb6d0c2fc66f205c2c533efb9fb
salagarsprabu <salagars2019@gmail.com>
2024-08-19 19:21:26 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,8 @@ def create_app():
     app.config.from_object('app.config.Config')
     db.init_app(app)
 
-    from .app import main_bp
+    from .routes import main_bp
     app.register_blueprint(main_bp)
 
     return app
+
