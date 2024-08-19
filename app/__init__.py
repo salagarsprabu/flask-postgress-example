5d105a5f65980b8b7ece496716be0e3f2676c6f8
salagarsprabu <salagars2019@gmail.com>
2024-08-19 19:07:54 +0530

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('app.config.Config')
+    db.init_app(app)
+
+    from .app import main_bp
+    app.register_blueprint(main_bp)
+
+    return app
