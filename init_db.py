0470838c4284827b057fe38a95902739d58ddaf9
salagarsprabu <salagars2019@gmail.com>
2024-08-19 19:23:22 +0530

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,5 +1,7 @@
-from app import db
-from app.models import User
+from app import create_app, db
 
-if __name__ == '__main__':
+app = create_app()
+
+with app.app_context():
     db.create_all()
+    print("Database initialized")
